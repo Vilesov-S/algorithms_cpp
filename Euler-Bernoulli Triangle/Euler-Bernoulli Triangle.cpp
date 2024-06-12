0e35a7a13a1d0828df42cec745a68545b2b16cd7
Vilesov-S <125795890+Vilesov-S@users.noreply.github.com>
2024-06-12 15:06:13 +0500

    рефакторинг

--- Euler-Bernoulli Triangle/Euler-Bernoulli Triangle.cpp
+++ Euler-Bernoulli Triangle/Euler-Bernoulli Triangle.cpp
@@ -3,7 +3,7 @@
 
 
 int main()
-{	
+{
 	std::cout << "Введите номер ряда = ";
 	int n;
 	std::cin >> n;
@@ -11,7 +11,7 @@ int main()
 	std::vector<std::vector<unsigned long long int>> tr(n, std::vector<unsigned long long int> (n, 0));
 	tr[0][0] = 1;
 	int i = 1, j = 0;
-	
+
 	int q = 1;
 	bool trend = true; // true - вверх, false - вниз
 	while (q < (n * (n + 1)/ 2))
@@ -20,7 +20,7 @@ int main()
 		{
 			if (j == 0) tr[i][j] = 0;
 			else tr[i][j] = tr[i + 1][j - 1] + tr[i][j - 1];
-			
+
 			if (i - 1 >= 0)	{ i--; j++; }
 			else { j++;	trend = false; }
 		}
@@ -28,18 +28,17 @@ int main()
 		{
 			if (i == 0) tr[i][j] = 0;
 			else tr[i][j] = tr[i - 1][j + 1] + tr[i - 1][j];
-			
+
 			if (j - 1 >= 0) { i++; j--; }
 			else { i++; trend = true; }
 		}
-		
 		q++;
 	}
-	
+
 	std::cout << "Последнее число в заданном ряду: ";
 	if (trend) std::cout << tr[i - 1][j];
 	else std::cout << tr[i][j - 1];
-	
+
 	std::cout << std::endl << "Треугольник: " << std::endl;;
 	for (int i = 0; i < n; i++)
 	{
@@ -51,6 +50,5 @@ int main()
 		std::cout << std::endl;
 	}
 }
-
 
 
