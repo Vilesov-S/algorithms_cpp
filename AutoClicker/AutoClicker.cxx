bc9f9244044a2342f397a5f66b5bda20dc80b42e
Vilesov-S <125795890+Vilesov-S@users.noreply.github.com>
2024-06-12 15:07:55 +0500

    autoclicker for windows

---
+++ AutoClicker/AutoClicker.cxx
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <thread>
+#include <chrono>
+
+int main()
+{
+    MessageBox(
+        NULL, 
+        "AutoClicker is running\n"
+        "Right_ALT - start/pause\n"
+        "END - to end the process",
+        "Start AutoClicker", MB_OK | MB_ICONINFORMATION);
+    
+    bool alt_pressed = false;
+    bool key_press = false;
+    while (true)
+    {
+        if (!key_press && GetAsyncKeyState(VK_RMENU) & 0x8000)
+        {
+            alt_pressed = !alt_pressed;
+        }
+        else if (!key_press && GetAsyncKeyState(VK_END) & 0x8000)
+        {
+            break;
+        }
+        key_press = (GetAsyncKeyState(VK_RMENU) & 0x8000);
+            
+        if (alt_pressed)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    
+    MessageBox(
+        NULL, 
+        "AutoClicker app has quit", 
+        "End AutoClicker", 
+        MB_OK | MB_ICONINFORMATION);
+
+    return 0;
+}
