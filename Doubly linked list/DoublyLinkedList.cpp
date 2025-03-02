00b97e99bb38662915b55e2635b6310cd8c9da26
twentyseven <vilesov_s@mail.ru>
2025-03-02 18:34:02 +0500

    refactoring

--- Doubly linked list/DoublyLinkedList.cpp
+++ Doubly linked list/DoublyLinkedList.cpp
@@ -14,23 +14,23 @@ private:
     DoubleLinkedListItem* pFirstItem;
     DoubleLinkedListItem* pLastItem;
     unsigned long long int size = 0;
-    
+
 public:
     DoubleLinkedList()
     : pFirstItem(nullptr)
     , pLastItem(nullptr)
     { }
-    
+
     bool is_empty()
     {
         return size == 0;
     }
-    
-    unsigned long long int getLenght()
+
+    unsigned long long int getLength()
     {
         return size;
     }
-    
+
     void push_front(int new_item)
     {
         DoubleLinkedListItem* tmp = new DoubleLinkedListItem;   // создание новой переменной
@@ -50,7 +50,7 @@ public:
         pFirstItem = tmp;                                        // новая переменная становится первой в списке (либо первой в изначально пустом);
         size++;                                                 // увеличение размера списка
     }
-    
+
     void push_back(int new_item)
     {
         DoubleLinkedListItem* tmp = new DoubleLinkedListItem;   // создание новой переменной
@@ -70,12 +70,12 @@ public:
         pLastItem = tmp; // новая переменная становится последней в списке (либо первой в изначально пустом);
         size++;
     }
-    
+
     void push_index(int new_item, long long int index = -1)
     {
         DoubleLinkedListItem* tmp = new DoubleLinkedListItem;
         tmp->item = new_item;
-        
+
         if (is_empty())
         {
             tmp->pNext = tmp;
@@ -124,15 +124,15 @@ public:
             {
                 tmp->pNext = curentItem;
                 tmp->pPrevious = curentItem->pPrevious;
-            
+
                 curentItem->pPrevious->pNext = tmp;
                 curentItem->pPrevious = tmp;
                 size++;
             }
         }
     }
-    
-    void pop_front()                                                                                      
+
+    void pop_front()
     {
         if (size == 1)
         {
@@ -152,7 +152,7 @@ public:
             size--;
         }
     }
-    
+
     void pop_back()
     {
         if (size == 1)
@@ -173,7 +173,7 @@ public:
             size--;
         }
     }
-    
+
     void pop_index(long long int index)
     {
         if (index < 0)
@@ -201,7 +201,7 @@ public:
             size--;
         }
     }
-    
+
     void displayList()
     {
         DoubleLinkedListItem* currentItem =  pFirstItem;
@@ -213,7 +213,7 @@ public:
         }
         std::cout << std::endl;
     }
-    
+
     DoubleLinkedListItem* getItem(long long int index)
     {
         DoubleLinkedListItem* currentItem = pFirstItem;
@@ -231,14 +231,14 @@ public:
         }
         return currentItem;
     }
-    
+
     void slideLeft(unsigned long long int step)
     {
         DoubleLinkedListItem* curentItem = getItem(step);
         pFirstItem = curentItem;
         pLastItem = curentItem->pPrevious;
     }
-    
+
     void slideRight(unsigned long long int step)
     {
         if (step >= size)
@@ -248,13 +248,13 @@ public:
         slideLeft(size - step);
     }
 };
-    
+
 
 
 int main()
 {
     DoubleLinkedList pDLL;
-    
+
     pDLL.push_back(4);
     pDLL.push_front(3);
     pDLL.push_back(5);
@@ -267,46 +267,46 @@ int main()
     pDLL.push_front(-1);
     pDLL.push_back(9);
     pDLL.push_back(10);
-    
-    
-    std::cout << "lenght = " << pDLL.getLenght() << ":" << std::endl;
+
+
+    std::cout << "length = " << pDLL.getLength() << ":" << std::endl;
     pDLL.displayList();
-    
+
     pDLL.pop_back();
-    std::cout << "lenght = " << pDLL.getLenght() << ":" << std::endl;
+    std::cout << "length = " << pDLL.getLength() << ":" << std::endl;
     pDLL.displayList();
-    
+
     pDLL.pop_front();
-    std::cout << "lenght = " << pDLL.getLenght() << ":" << std::endl;
+    std::cout << "length = " << pDLL.getLength() << ":" << std::endl;
     pDLL.displayList();
-    
+
     std::cout << "item 24 = " << pDLL.getItem(24)->item << std::endl;
     std::cout << "item -26 = " << pDLL.getItem(-26)->item << std::endl;
-    
+
     pDLL.push_index(666, 5);
-    std::cout << "lenght = " << pDLL.getLenght() << ":" << std::endl;
+    std::cout << "length = " << pDLL.getLength() << ":" << std::endl;
     pDLL.displayList();
-    
+
     pDLL.pop_index(5);
-    std::cout << "lenght = " << pDLL.getLenght() << ":" << std::endl;
+    std::cout << "length = " << pDLL.getLength() << ":" << std::endl;
     pDLL.displayList();
-    
+
     pDLL.slideRight(4);
-    std::cout << "lenght = " << pDLL.getLenght() << ":" << std::endl;
+    std::cout << "length = " << pDLL.getLength() << ":" << std::endl;
     pDLL.displayList();
-    
+
     pDLL.slideLeft(4);
-    std::cout << "lenght = " << pDLL.getLenght() << ":" << std::endl;
+    std::cout << "length = " << pDLL.getLength() << ":" << std::endl;
     pDLL.displayList();
-    
+
     // std::cout << std::endl;
     // std::cout << std::endl;
-    // for (int i = 0; i < pDLL.getLenght(); i++)
+    // for (int i = 0; i < pDLL.getLength(); i++)
     // {
-        
+
         // std::cout << pDLL.getItem(i)->pPrevious->item << " " << pDLL.getItem(i)->item << " " << pDLL.getItem(i)->pNext->item << std::endl;
     // }
-    
+
     // std::cout << std::endl;
     // std::cout << std::endl;
     // DoubleLinkedList test_list = DoubleLinkedList();
@@ -316,18 +316,18 @@ int main()
         // test_list.push_index(i, n);
         // test_list.displayList();
     // }
-    
+
     // std::cout << std::endl;
-    
+
     // for(int i = 1; i <= n; i++)
     // {
         // test_list.pop_front();
         // test_list.displayList();
     // }
-    
+
     // std::cout << test_list.is_empty() << std::endl;
-    
-    
+
+
     return 0;
 };
 
